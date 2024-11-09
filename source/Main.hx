177beaed446612bd988d098cc848c43ef285958d
Percossi283 <eros.percossi@gmail.com>
2024-11-09 17:15:13 +0100

    Initialize mobile

---
+++ source/Main.hx
@@ -0,0 +1,204 @@
+package;
+
+import flixel.FlxGame;
+import openfl.Lib;
+import openfl.display.Sprite;
+import debug.FPSCounter;
+import lime.app.Application;
+import backend.SSPlugin as ScreenShotPlugin;
+#if mobile
+import mobile.CopyState;
+#end
+
+#if linux
+import lime.graphics.Image;
+#end
+
+using StringTools;
+
+#if linux
+@:cppInclude('./external/gamemode_client.h')
+@:cppFileCode('
+	#define GAMEMODE_AUTO
+')
+#end
+class Main extends Sprite {
+	final game = {
+		width: 1280,
+		height: 720,
+		initialState: InitState.new,
+		zoom: -1.0,
+		framerate: 60,
+		skipSplash: true,
+		startFullscreen: false
+	};
+
+	public static var fpsVar:FPSCounter;
+
+	public static final superDangerMode:Bool = Sys.args().contains("-troll");
+
+    public static final __superCoolErrorMessagesArray:Array<String> = [
+        "A fatal error has occ- wait what?",
+        "missigno.",
+        "oopsie daisies!! you did a fucky wucky!!",
+        "i think you fogot a semicolon",
+        "null balls reference",
+        "get friday night funkd'",
+        "engine skipped a heartbeat",
+        "Impossible...",
+        "Patience is key for success... Don't give up.",
+        "It's no longer in its early stages... is it?",
+        "It took me half a day to code that in",
+        "You should make an issue... NOW!!",
+        "> Crash Handler written by: ur mom",
+        "broken ch-... wait what are we talking about",
+        "could not access variable you.dad",
+        "What have you done...",
+		"Sounds like skill issue to me-",
+        "THERE ARENT COUGARS IN SCRIPTING!!! I HEARD IT!!",
+        "no, thats not from system.windows.forms",
+        "you better link a screenshot if you make an issue, or at least the crash.txt",
+        "stack trace more like dunno i dont have any jokes",
+        "oh the misery. everybody wants to be my enemy",
+        "have you heard of soulles dx",
+        "i thought it was invincible",
+        "did you deleted coconut.png",
+        "have you heard of missing json's cousin null function reference",
+        "sad that linux users wont see this banger of a crash handler",
+        "woopsie",
+        "oopsie",
+        "woops",
+        "silly me",
+		"meow",
+        "my bad",
+        "first time, huh?",
+        "did somebody say yoga",
+        "we forget a thousand things everyday... make sure this is one of them.",
+        "SAY GOODBYE TO YOUR KNEECAPS, CHUCKLEHEAD",
+        "motherfucking ordinal 344 (TaskDialog) forcing me to create a even fancier window",
+        //'Died due to missing a sawblade. (Press ${mobile.MobileControls.enabled ? 'Extra ${mobile.MobileControls.mode == "Hitbox" ? 'Hint' : 'Button'}' : 'Space'} to dodge!)',
+        "yes rico, kaboom.",
+        "hey, while in freeplay, press shift while pressing space",
+        "goofy ahh engine",
+        //'pssst, try ${mobile.MobileControls.enabled ? 'pressing back' : 'typing debug7'} in the options menu',
+        "this crash handler is sponsored by rai-",
+        "",
+        "did you know a jiffy is an actual measurement of time",
+        "how many hurt notes did you put",
+        "FPS: 0",
+        "\r\ni am a secret message",
+		"what is a crash handler?",
+        "this is garnet",
+        "Error: Sorry i already have a girlfriend",
+        "did you know theres a total of 51 silly messages",
+        "whoopsies looks like i forgot to fix this",
+        "Game used Crash. It's super effective!"
+    ];
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+	public static function main():Void {
+		Lib.current.addChild(new Main());
+	}
+
+	public function new() {
+		super();
+		#if mobile
+		#if android
+		SUtil.doPermissionsShit();
+		#end
+		Sys.setCwd(SUtil.getStorageDirectory());
+		#end
+
+		CrashHandler.init();
+
+		#if windows //DPI AWARENESS BABY
+		@:functionCode('
+		#include <Windows.h>
+		SetProcessDPIAware()
+		DisableProcessWindowsGhosting()
+		')
+		#end
+		setupGame();
+	}
+
+	public static var askedToUpdate:Bool = false;
+
+	private function setupGame():Void {
+		#if (openfl <= "9.2.0")
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (game.zoom == -1.0) {
+			var ratioX:Float = stageWidth / game.width;
+			var ratioY:Float = stageHeight / game.height;
+			game.zoom = Math.min(ratioX, ratioY);
+			game.width = Math.ceil(stageWidth / game.zoom);
+			game.height = Math.ceil(stageHeight / game.zoom);
+		};
+		#end
+
+		// #if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
+		ClientPrefs.loadDefaultStuff();
+		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
+
+		addChild(new FlxGame(game.width, game.height, #if (mobile && MODS_ALLOWED) !CopyState.checkExistingFiles() ? CopyState : #end game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+
+		fpsVar = new FPSCounter(3, 3, 0x00FFFFFF);
+		addChild(fpsVar);
+
+		if (fpsVar != null) {
+			fpsVar.visible = ClientPrefs.showFPS;
+		}
+
+		#if (!web && flixel < "5.5.0")
+		FlxG.plugins.add(new ScreenShotPlugin());
+		#elseif (flixel >= "5.6.0")
+		FlxG.plugins.addIfUniqueType(new ScreenShotPlugin());
+		#end
+
+		FlxG.autoPause = false;
+
+		#if linux
+		var icon = Image.fromFile("icon.png");
+		Lib.current.stage.window.setIcon(icon);
+		#end
+
+		#if mobile
+		lime.system.System.allowScreenTimeout = ClientPrefs.screensaver;
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK]; 
+		#end
+		#end
+
+		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+		});
+
+		#if DISCORD_ALLOWED DiscordClient.prepare(); #end
+
+		// shader coords fix
+		FlxG.signals.gameResized.add(function(w, h) {
+			if (FlxG.cameras != null) {
+			  	for (cam in FlxG.cameras.list) {
+			   		if (cam != null && cam.filters != null)
+				   		resetSpriteCache(cam.flashSprite);
+			  	}
+		   	}
+
+		   if (FlxG.game != null) resetSpriteCache(FlxG.game);
+	   });
+	}
+
+	static function resetSpriteCache(sprite:Sprite):Void {
+		@:privateAccess {
+		    sprite.__cacheBitmap = null;
+			sprite.__cacheBitmapData = null;
+		}
+	}
+
+	public static function changeFPSColor(color:FlxColor) {
+		fpsVar.textColor = color;
+	}
+}
